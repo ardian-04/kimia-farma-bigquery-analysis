35c38f50429aea96f66a91819ba9986fbcd4234e
ardian-04 <ardianmuhammad1933@gmail.com>
2026-02-24 22:44:17 +0700

    Create create_tabel_analisa.sql

---
+++ sql/create_tabel_analisa.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE TABLE
+  `rakamin-kf-analytics-488209.Rakamin_KF_Analytics.tabel_analisa` AS
+
+SELECT
+  -- Identitas Transaksi
+  t.transaction_id,
+  t.date,
+  t.customer_name,
+  t.rating AS rating_transaksi,
+
+  -- Informasi Cabang
+  kc.branch_id,
+  kc.branch_name,
+  kc.kota,
+  kc.provinsi,
+  kc.rating AS rating_cabang,
+
+  -- Informasi Produk
+  p.product_id,
+  p.product_name,
+  p.price AS actual_price,
+
+  -- Diskon
+  t.discount_percentage,
+
+  -- Persentase Gross Laba
+  CASE
+    WHEN p.price <= 50000 THEN 0.10
+    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
+    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    ELSE 0.30
+  END AS persentase_gross_laba,
+
+  -- Nett Sales
+  p.price * (1 - t.discount_percentage / 100) AS nett_sales,
+
+  -- Nett Profit
+  (p.price * (1 - t.discount_percentage / 100)) *
+  CASE
+    WHEN p.price <= 50000 THEN 0.10
+    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
+    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    ELSE 0.30
+  END AS nett_profit
+
+FROM
+  `rakamin-kf-analytics-488209.Rakamin_KF_Analytics.kf_final_transactions` t
+
+LEFT JOIN
+  `rakamin-kf-analytics-488209.Rakamin_KF_Analytics.kf_kantor_cabang` kc
+ON
+  t.branch_id = kc.branch_id
+
+LEFT JOIN
+  `rakamin-kf-analytics-488209.Rakamin_KF_Analytics.kf_product` p
+ON
+  t.product_id = p.product_id;
